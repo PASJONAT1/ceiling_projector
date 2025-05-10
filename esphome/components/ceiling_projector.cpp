3971d378b13a8f3d4ded4f1e5fea5980be86b51c
PASJONAT1 <65369701+PASJONAT1@users.noreply.github.com>
2025-05-10 22:13:48 +0200

    Update ceiling_projector.cpp

--- esphome/components/ceiling_projector.cpp
+++ esphome/components/ceiling_projector.cpp
@@ -4,8 +4,14 @@ cpp
 #include "esphome.h"
 
 class CeilingProjector : public Component {
-  public:
-    CeilingProjector(GPIOPin *cs_pin, GPIOPin *clk_pin, GPIOPin *data_pin);
+        public:
+          CeilingProjector(GPIOPin *cs_pin, GPIOPin *clk_pin, GPIOPin *data_pin)
+              : cs_pin_(cs_pin), clk_pin_(clk_pin), data_pin_(data_pin) {
+            hours_ = 12;
+            minutes_ = 34;
+            brightness_ = 0;
+            colon_enabled_ = true;
+            display_mode_ = 0; // 0 = normalny czas, 1 = czas zmywarki
     void setup() override;
     void loop() override;
 };
