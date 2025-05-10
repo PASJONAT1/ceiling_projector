056e2e817f3132766129d6090bdd688f4c9c437f
PASJONAT1 <65369701+PASJONAT1@users.noreply.github.com>
2025-05-10 22:12:10 +0200

    Update ceiling_projector.h

--- esphome/components/ceiling_projector.h
+++ esphome/components/ceiling_projector.h
@@ -4,15 +4,7 @@ cpp
 #include "esphome.h"
 
   - lambda: |-
-      class CeilingProjector : public Component {
-        public:
-          CeilingProjector(GPIOPin *cs_pin, GPIOPin *clk_pin, GPIOPin *data_pin)
-              : cs_pin_(cs_pin), clk_pin_(clk_pin), data_pin_(data_pin) {
-            hours_ = 12;
-            minutes_ = 34;
-            brightness_ = 0;
-            colon_enabled_ = true;
-            display_mode_ = 0; // 0 = normalny czas, 1 = czas zmywarki
+      
             
             // Inicjalizacja statusu
             status_ = "Initializing";
